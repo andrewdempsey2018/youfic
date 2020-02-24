1db4d7d85abfa1bf2fc0b23ea17531f723211dca
Andrew Dempsey <andrewdempsey2016@gmail.com>
2020-02-24 18:40:49 +0000

    Refactor accounts to reflect django 2 syntax

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url, include
+from django.urls import path, include
 from accounts.views import index, logout, login, registration, user_profile
 
 urlpatterns = [
-    url(r'^logout/$', logout, name="logout"),
-    url(r'^login/$', login, name="login"),
-    url(r'^register/$', registration, name="registration"),
-    url(r'^profile/$', user_profile, name="profile"),
+    path('logout/', logout, name="logout"),
+    path('login/', login, name="login"),
+    path('register/', registration, name="registration"),
+    path('profile/', user_profile, name="profile"),
 ]
