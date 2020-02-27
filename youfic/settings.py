7f6239923a28a63a3b85d28fabe6b92a4840638d
andrewdempsey2018 <38543181+andrewdempsey2018@users.noreply.github.com>
2020-02-27 18:37:26 +0000

    Removed env import for heroku hosting

--- youfic/settings.py
+++ youfic/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 # Add enviornmant variables
-import env
+#import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
