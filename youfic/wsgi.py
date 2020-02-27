65f513d06dc659cc9f04f99c2663ac79f20067b8
Andrew Dempsey <andrewdempsey2016@gmail.com>
2020-02-27 19:21:06 +0000

    Removed error from wsgi.py

--- youfic/wsgi.py
+++ youfic/wsgi.py
@@ -1,8 +1,6 @@
 """
 WSGI config for youfic project.
-
 It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
@@ -10,12 +8,7 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'youfic.settings')
 
 application = get_wsgi_application()
-
-# Enable whitenoise for static file hosting on Heroku
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
