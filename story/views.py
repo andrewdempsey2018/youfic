174c36c498f71d53a56e99b1f93c7c5311d71885
Andrew Dempsey <andrewdempsey2016@gmail.com>
2020-02-28 20:46:02 +0000

    Added submit story form to submit page

--- story/views.py
+++ story/views.py
@@ -4,31 +4,52 @@ from django.http import HttpResponse
 # Import the Story model for use
 from .models import Story
 
+
 def index(request):
     # Query the database and pull out a set of all the stories it contains
     stories = Story.objects.all()
     # Pass the set of stories to the 'index' template
-    return render(request, 'story/index.html', { 'stories': stories })
+    return render(request, "story/index.html", {"stories": stories})
+
 
 def adventure(request):
-    stories = Story.objects.filter(category='Adventure')
-    return render(request, 'story/index.html', { 'stories': stories })
+    stories = Story.objects.filter(category="Adventure")
+    return render(request, "story/index.html", {"stories": stories})
+
 
 def fantasy(request):
-    stories = Story.objects.filter(category='Fantasy')
-    return render(request, 'story/index.html', { 'stories': stories })
+    stories = Story.objects.filter(category="Fantasy")
+    return render(request, "story/index.html", {"stories": stories})
+
 
 def mystery(request):
-    stories = Story.objects.filter(category='Mystery')
-    return render(request, 'story/index.html', { 'stories': stories })
+    stories = Story.objects.filter(category="Mystery")
+    return render(request, "story/index.html", {"stories": stories})
+
 
 def scifi(request):
-    stories = Story.objects.filter(category='SciFi')
-    return render(request, 'story/index.html', { 'stories': stories })
+    stories = Story.objects.filter(category="SciFi")
+    return render(request, "story/index.html", {"stories": stories})
+
 
 def read(request):
-    story = Story.objects.get(id=request.GET.get('id'))
-    return render(request, 'story/read.html', { 'story': story })
+    story = Story.objects.get(id=request.GET.get("id"))
+    return render(request, "story/read.html", {"story": story})
+
 
 def submit(request):
-    return render(request, 'story/submit.html')
+    return render(request, "story/submit.html")
+
+def submit(request):
+    if request.method == "POST":
+        if request.POST.get("title") and request.POST.get("author"):
+            story = Story()
+            story.title = request.POST.get("title")
+            story.author = request.POST.get("author")
+            story.save()
+
+            return render(request, "story/submit.html")
+
+    else:
+        return render(request, "story/submit.html")
+
