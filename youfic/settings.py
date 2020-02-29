69b7dea17aa836c8fc4b33e6af4e6f8c4bd1d458
Andrew Dempsey <andrewdempsey2016@gmail.com>
2020-02-29 13:52:48 +0000

    Updated submit story page

--- youfic/settings.py
+++ youfic/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 # Add enviornmant variables
-#import env
+import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'youfic.herokuapp.com']
 
